ec74447d8d44bae785041327ae2898f7b49866f6
cairomurphy <84107491+cairomurphy@users.noreply.github.com>
2022-02-07 09:40:57 -0800

    cleaned up the project

--- gitfun2/Controllers/HomeController.cs
+++ gitfun2/Controllers/HomeController.cs
@@ -11,11 +11,9 @@ namespace gitfun2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,15 +21,6 @@ namespace gitfun2.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
